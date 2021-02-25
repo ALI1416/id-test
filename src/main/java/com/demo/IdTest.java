3f064e179b07bea539399aa25b3df7e3db596bfc
Ali Cheng/WZS/Wistron <Ali_Cheng@wistron.com>
2021-02-25 08:57:01 +0800

    0225

---
+++ src/main/java/com/demo/IdTest.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+import cn.z.id.Id;
+
+/**
+ * <h1>高性能Id生成器测试</h1>
+ *
+ * <p>
+ * createDate 2021/02/25 08:52:28
+ * </p>
+ *
+ * @author ALI[ali-k@foxmail.com]
+ * @since 1.0.0
+ **/
+public class IdTest {
+
+    public static void main(String[] args) {
+        System.out.println(Id.next());
+    }
+
+}
